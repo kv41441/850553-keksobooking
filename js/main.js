ac4f621431c2183a03120fe5e6cc18d495b5ff95
sithari41441 <42210922+sithari41441@users.noreply.github.com>
2018-12-23 00:58:41 +0700

    Выполнено 2е задание 4го модуля (кроме п.1.7 ТЗ)

--- js/main.js
+++ js/main.js
@@ -24,6 +24,10 @@ var newCard = document.querySelector('.map');
 var filtersContainer = document.querySelector('.map__filters-container');
 var mapPinMain = document.querySelector('.map__pin--main');
 var addressInput = document.querySelector('#address');
+var houseTypeSelect = document.querySelector('#type');
+var timeSelect = document.querySelector('.ad-form__element--time');
+var roomNumberSelect = document.querySelector('#room_number');
+var resetButton = document.querySelector('.ad-form__reset');
 
 
 var showInitialPinCoordinates = function () {
@@ -45,6 +49,105 @@ var closeCard = function () {
   });
 };
 
+var setMinPrice = function () {
+  var priceInput = document.querySelector('#price');
+  var houseType = document.querySelectorAll('#type option');
+
+  houseType.forEach(function (item) {
+    if (item.selected) {
+      switch (item.value) {
+        case 'bungalo':
+          priceInput.placeholder = '0';
+          priceInput.min = 0;
+          break;
+        case 'flat':
+          priceInput.placeholder = '1000';
+          priceInput.min = 1000;
+          break;
+        case 'house':
+          priceInput.placeholder = '5000';
+          priceInput.min = 5000;
+          break;
+        case 'palace':
+          priceInput.placeholder = '10000';
+          priceInput.min = 10000;
+          break;
+      }
+    }
+  });
+};
+
+var timeSync = function (target) {
+  var timeIn = document.querySelectorAll('#timein option');
+  var timeOut = document.querySelectorAll('#timeout option');
+
+  if (target === 'timein') {
+    timeIn.forEach(function (item, i) {
+      if (item.selected) {
+        timeOut[i].selected = true;
+      }
+    });
+  } else if (target === 'timeout') {
+    timeOut.forEach(function (item, i) {
+      if (item.selected) {
+        timeIn[i].selected = true;
+      }
+    });
+  }
+};
+
+var houseCapacityChange = function () {
+  var roomNumber = document.querySelectorAll('#room_number option');
+  var capacity = document.querySelectorAll('#capacity option');
+
+  capacity.forEach(function (capItem) {
+    capItem.disabled = false;
+  });
+
+  roomNumber.forEach(function (roomItem) {
+    if (roomItem.selected) {
+      switch (roomItem.value) {
+        case '1':
+          capacity.forEach(function (capItem) {
+            if (capItem.value !== '1') {
+              capItem.disabled = true;
+            } else {
+              capItem.selected = true;
+            }
+          });
+          break;
+        case '2':
+          capacity.forEach(function (capItem) {
+            if (capItem.value !== '1' && capItem.value !== '2') {
+              capItem.disabled = true;
+            } else {
+              capacity[1].selected = true;
+            }
+          });
+          break;
+        case '3':
+          capacity.forEach(function (capItem) {
+            if (capItem.value === '0') {
+              capItem.disabled = true;
+            } else {
+              capacity[0].selected = true;
+            }
+          });
+          break;
+        case '100':
+          capacity.forEach(function (capItem) {
+            if (capItem.value !== '0') {
+              capItem.disabled = true;
+            } else {
+              capItem.selected = true;
+            }
+          });
+          break;
+      }
+    }
+  });
+};
+
 var showMap = function () {
   var mapFilters = document.querySelectorAll('.map__filters select, .map__filters fieldset, .ad-form fieldset');
 
@@ -56,8 +159,10 @@ var showMap = function () {
   });
 };
 
-/* var hideMap = function () {
+var hideMap = function () {
   var mapFilters = document.querySelectorAll('.map__filters select, .map__filters fieldset, .ad-form fieldset');
+  var mapPins = mapPinListElement.querySelectorAll('.map__pin');
+  var initialMapPin = mapPinListElement.querySelector('.map__pin--main');
 
   document.querySelector('.map').classList.add('map--faded');
   document.querySelector('.ad-form').classList.add('ad-form--disabled');
@@ -65,7 +170,13 @@ var showMap = function () {
   mapFilters.forEach(function (item) {
     item.disabled = true;
   });
-};*/
+
+  mapPins.forEach(function (item) {
+    if (item !== initialMapPin) {
+      mapPinListElement.removeChild(item);
+    }
+  });
+};
 
 var getRandomElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -261,6 +372,8 @@ var showPinCoodrinates = function () {
 };
 
 showInitialPinCoordinates();
+setMinPrice();
+houseCapacityChange();
 
 var completeOffers = createCompleteOffer(8);
 
@@ -268,4 +381,22 @@ mapPinMain.addEventListener('mouseup', function () {
   showMap();
   renderMapPin();
   showPinCoodrinates();
+});
+
+houseTypeSelect.addEventListener('change', function () {
+  setMinPrice();
+});
+
+timeSelect.addEventListener('change', function (evt) {
+  var target = evt.target.id;
+
+  timeSync(target);
+});
+
+roomNumberSelect.addEventListener('change', function () {
+  houseCapacityChange();
+});
+
+resetButton.addEventListener('click', function () {
+  hideMap();
 });
